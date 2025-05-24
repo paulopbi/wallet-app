814751c63e37bd43f2d6e7a2296c8455c1c89930
Paulo Victor Silva <paulpbi16@gmail.com>
2025-05-24 16:58:25 -0300

    feat: add get, post and delete routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { sql } from "./config/db.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 async function initiDB() {
   try {
@@ -24,8 +25,65 @@ async function initiDB() {
   }
 }
 
+app.get("/api/transactions/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const transactions = await sql`
+    SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+    `;
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error to get the transaction", error);
+    return res.status(500).json({ message: error });
+  }
+});
+
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { title, amount, category, user_id } = req.body;
+
+    if (!title || !user_id | !category || amount === undefined) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const transaction = await sql`
+    INSERT INTO transactions(user_id, title, amount, category)
+    VALUES (${user_id}, ${title}, ${amount}, ${category})
+    RETURNING *
+    `;
+    res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.log("Error to creating the transaction", error);
+    return res.status(500).json({ message: error });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction id" });
+    }
+
+    const result = await sql`
+    DELETE FROM transactions WHERE id = ${id} RETURNING * 
+    `;
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res.status(200).json({ message: "Transaction deleted sucessfully" });
+  } catch (error) {
+    console.log("Error to delete the transaction", error);
+    return res.status(500).json({ message: error });
+  }
+});
+
 app.get("/", (req, res) => {
-  res.send("Working");
+  return res.send("Working");
 });
 
 initiDB().then(() => {

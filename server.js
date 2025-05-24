24d9b17cf993ce7984b673493b86031f02b5419d
Paulo Victor Silva <paulpbi16@gmail.com>
2025-05-24 15:57:59 -0300

    feat: add database settings

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
-const app = express();
 dotenv.config();
+const app = express();
+
+async function initiDB() {
+  try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10,2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )
+    `;
+    console.log("Database initialized sucessfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1);
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Working");
 });
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server is up on port ${PORT}`);
+initiDB().then(() => {
+  const PORT = process.env.PORT || 5001;
+  app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}`);
+  });
 });
